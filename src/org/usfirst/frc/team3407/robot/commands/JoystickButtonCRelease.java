992ecbbe91c417c06f2b4859afdec26fec40ce11
Michael <mslucke99@gmail.com>
2017-02-10 09:45:13 -0600

    Update JoystickButtonCRelease.java

--- src/org/usfirst/frc/team3407/robot/commands/JoystickButtonCRelease.java
+++ src/org/usfirst/frc/team3407/robot/commands/JoystickButtonCRelease.java
@@ -20,6 +20,7 @@ public class JoystickButtonCRelease extends Command {
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
     	OI.JBCIsHeld = false;   	
+        SmartDashboard.putBoolean("DB/LED 2", OI.JBCIsHeld);
     }
 
     // Make this return true when this Command no longer needs to run execute()
