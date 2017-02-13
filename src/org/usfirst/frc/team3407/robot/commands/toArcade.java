570a965cb101c3a81589dfbeeefe62fbf133dac9
Michael <mslucke99@gmail.com>
2017-02-13 07:59:55 -0600

    Update toArcade.java

--- src/org/usfirst/frc/team3407/robot/commands/toArcade.java
+++ src/org/usfirst/frc/team3407/robot/commands/toArcade.java
@@ -20,7 +20,7 @@ public class JoystickButtonCHeld extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	OI.isArcade = true
+    	OI.isArcade = true;
     }
 
     // Make this return true when this Command no longer needs to run execute()
