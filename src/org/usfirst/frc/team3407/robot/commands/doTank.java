0d9a0a7587ef58dec524ec4d20236ecb626f3b09
MustyBot <mslucke99@gmail.com>
2017-03-15 12:41:46 -0500

    another attempt at tankade

---
+++ src/org/usfirst/frc/team3407/robot/commands/doTank.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team3407.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team3407.robot.OI;
+
+/**
+ *
+ */
+public class doTank extends Command {
+
+    public doTank() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	OI.toTank();
+    	OI.noSlow();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	if(!OI.isArcade() && OI.isTankSlow()){
+    		return true;
+    	}
+    	else{
+    		return false;
+    	}
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
